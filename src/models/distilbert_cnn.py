fa6438b4931260715a904ed3c299b4e33266af80
Fiorenzo <fiorenzo.parascandolo@gmail.com>
2021-02-08 22:19:08 +0100

    fixed CNN

--- src/models/distilbert_cnn.py
+++ src/models/distilbert_cnn.py
@@ -7,16 +7,17 @@ from models import DistilBertWithOutputKnowledge
 class DistilBertCNN(DistilBertWithOutputKnowledge):
     def __init__(self, model_info=ModelInfo('distilbert-base-uncased'), alpha=0.75, alpha_step=0):
         super(DistilBertCNN, self).__init__(model_info, alpha, alpha_step)
-        self.conv = nn.Conv2d(in_channels=self.info.embedding_dim, out_channels=self.info.embedding_dim,
-                              kernel_size=(1, 2), stride=(1, 4))
+        self.conv = nn.Conv1d(in_channels=self.info.embedding_dim, out_channels=self.info.embedding_dim, kernel_size=2)
 
     def _logits(self, x):
         x = self.encoder(input_ids=x[:, 0], attention_mask=x[:, 1], output_hidden_states=True)
         x_hidden_states = x["hidden_states"]
         x = torch.stack([x_hidden_state for x_hidden_state in x_hidden_states[-4:]], dim=3)
-        x = x.transpose(1, 2)
-        x = self.conv(x).squeeze(dim=3)
-        x = x.transpose(1, 2)
+        batch_length = x.shape[0]
+        x = x.view(x.shape[0] * x.shape[1], x.shape[2], x.shape[3])
+        x = self.conv(x)
+        x = torch.max(x, dim=2)[0]
+        x = x.view(batch_length, self.info.max_length, self.info.embedding_dim)
         start = self.start_fc(x).squeeze(dim=2)
         end = self.end_fc(x).squeeze(dim=2)
         return start, end
